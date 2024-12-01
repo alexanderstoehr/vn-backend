be8e602f80de04513c5041c8e7060c22a9d44c7e
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-01 20:05:57 +0100

    added user profile model and admin view

--- apps/user_profile/admin.py
+++ apps/user_profile/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from apps.user_profile.models import UserProfile
+
 
 # Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in UserProfile._meta.fields]
--- apps/user_profile/models.py
+++ apps/user_profile/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model() # get the current user model
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='user_profile')
+
+    is_following = models.ManyToManyField('self', symmetrical=False, related_name='followed_by')
+    about_me = models.TextField(blank=True, null=True, max_length=1500)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.email}'s profile"
