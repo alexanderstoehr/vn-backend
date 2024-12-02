425ab3b6d8e0a3658c5f6e206872dca9e6064ccf
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-02 21:58:01 +0100

    added category endpoints for adding and replacing categories from video

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from apps.category.views import ListAllCategories
+from apps.category.views import ListAllCategories, AttachCategoryToVideo
 
 urlpatterns = [
     path('', ListAllCategories.as_view(), name='list-all-categories'),
+    path("attach-category/", AttachCategoryToVideo.as_view(), name="attach-category-to-video"),
 ]
--- apps/category/views.py
+++ apps/category/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from unicodedata import category
+
+from rest_framework import status
 from rest_framework.generics import ListAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from apps.category.models import Category
 from apps.category.serializer import CategorySerializer
+from apps.video.models import Video
 
 
 # Create your views here.
@@ -11,3 +16,27 @@ from apps.category.serializer import CategorySerializer
 class ListAllCategories (ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class AttachCategoryToVideo(APIView):
+    def post(self, request, *args, **kwargs):
+        category_id = request.data.get('category_id')
+        video_id = request.data.get('video_id')
+
+        if not category_id or not video_id:
+            return Response({'error': 'Category ID and video ID are required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            video = Video.objects.get(id=video_id)
+        except Video.DoesNotExist:
+            return Response({'error': 'Video not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            return Response({'error': 'Category not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        video.category = category
+        video.save()
+
+        return Response({'message': 'Category attached to video'}, status=status.HTTP_200_OK)
