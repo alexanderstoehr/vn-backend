3076be155a7e06a2f5dbec10067f5ad68a7ee7b0
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-04 14:05:02 +0100

    added space endpoints

--- apps/space/views.py
+++ apps/space/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+
+from apps.space.models import Space
+from apps.space.serializer import SpaceSerializer
+
 
 # Create your views here.
 
 # # GET all spaces and its videos from current user
+# # POST a new space
+
+class ListCreateSpaceView(ListCreateAPIView):
+    serializer_class = SpaceSerializer
+    queryset = Space.objects.all()
+
 
 # # GET a single space and its videos
 # # PATCH an existing space
-# # POST a new space
 # # DELETE an existing space
+
+
+class RetrieveUpdateDeleteSpaceView(RetrieveUpdateDestroyAPIView):
+    serializer_class = SpaceSerializer
+    queryset = Space.objects.all()
--- project/urls.py
+++ project/urls.py
@@ -37,8 +37,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path("api/categories/", include("apps.category.urls")),
-    path('api/', include('apps.tag.urls')),  # Include the tag app URLs
-    path('api/', include('apps.note.urls')),  # Include the user app URLs
-    path('api/', include('apps.video.urls')),  # Include the user app URLs
+    path("api/categories/", include("apps.category.urls")), # Include the Category app URLs
+    path('api/', include('apps.tag.urls')),  # Include the Tag app URLs
+    path('api/', include('apps.note.urls')),  # Include the Note app URLs
+    path('api/', include('apps.video.urls')),  # Include the Video app URLs
+    path('api/', include('apps.space.urls')),  # Include the Space app URLs
     ]
