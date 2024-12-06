8c29f632ee0c03e794480772330e65c32dad86f8
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-06 10:27:43 +0100

    cleaned nested user serializer data to not have email adresses

--- apps/user_profile/serializer.py
+++ apps/user_profile/serializer.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
+
+from apps.basic_user.serializer import UserSerializer
 from apps.user_profile.models import UserProfile
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
 
     class Meta:
         model = UserProfile
-        fields = ["user", "external_user_id", "about_me"]
+        fields = ["id", "external_user_id"]
 
 class UserProfileVideoSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField()
 
     class Meta:
         model = UserProfile
-        fields = ["user", "id", "external_user_id"]
+        fields = [ "id", "external_user_id"]
