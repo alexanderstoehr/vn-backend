dda34bd620044c166cbf3d51ba9fb58602c7a01c
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-08 18:43:24 +0100

    added http to localhost domain to allowed cors origins

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ['localhost', "127.0.0.1", os.environ.get("FRONTEND_DOMAIN")]
 
-CORS_ALLOWED_ORIGINS = ['localhost', "127.0.0.1", os.environ.get("FRONTEND_DOMAIN")]
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1", os.environ.get("FRONTEND_DOMAIN")]
 
 
 SERVER_TYPE = os.environ.get("SERVER_TYPE", "local")
