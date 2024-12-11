1e34c9a9727d160131744df2c258d64b044a7c27
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-11 17:59:39 +0100

    set firstname and lastname as optional fields

--- apps/basic_user/models.py
+++ apps/basic_user/models.py
@@ -6,3 +6,5 @@ class User (AbstractUser):
     email = models.EmailField(unique=True)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
