c4fa32b1dfe27a47f7db3bd65d295f5ccd0f6819
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-11 18:51:39 +0100

    added firstname and lastname fields to serializer fields

--- apps/basic_user/serializer.py
+++ apps/basic_user/serializer.py
@@ -6,7 +6,7 @@ from apps.basic_user.admin import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = ['id', 'username', 'email', 'first_name', 'last_name']
         extra_kwargs = {
             'first_name': {'required': False},
             'last_name': {'required': False},
