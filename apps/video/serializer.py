96bb37bdc7b7273dc98d0deeaede9e28cd82eec9
Alexander Stöhr <git@alexander-stoehr.de>
2024-12-17 17:25:06 +0100

    added notes in video serializer

--- apps/video/serializer.py
+++ apps/video/serializer.py
@@ -8,7 +8,7 @@ from rest_framework import serializers
 
 class VideoSerializer(serializers.ModelSerializer):
     video_owner = UserProfileVideoSerializer()
-    #notes = NoteSerializer(many=True, read_only=True)
+    notes = NoteSerializer(many=True, read_only=True)
     tags = TagSerializer(many=True, read_only=True)
     category = CategorySerializer()
     notes_count = serializers.SerializerMethodField()
@@ -17,7 +17,7 @@ class VideoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Video
-        fields = ["id", "video_title", "video_description","video_host_url", "video_host_id","video_host_thumbnail_url","created_at","video_owner", "notes_count", "category","tags_count", "tags"]
+        fields = ["id", "video_title", "video_description","video_host_url", "video_host_id","video_host_thumbnail_url","created_at","video_owner", "notes_count", "notes","category","tags_count", "tags"]
 
 
     def get_notes_count(self, obj):
@@ -34,7 +34,6 @@ class VideoCreateSerializer(serializers.ModelSerializer):
 
         extra_kwargs = {
             "space": {"required": True},
-            "video_title": {"required": True},
             "video_title": {"required": True},
             "video_description": {"required": True},
             "video_host_url": {"required": True},
